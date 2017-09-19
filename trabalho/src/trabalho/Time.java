32a4448e5feb226d7d344f3254d6de4b6d23666d
CesarFilho7 <cesarfec7@gmail.com>
2017-09-19 15:09:33 -0300

    trabalho poo

---
+++ trabalho/src/trabalho/Time.java
@@ -0,0 +1,14 @@
+package trabalho;
+
+public class Time {
+	public String nome;
+	public int forca;
+	
+	public Time(String nome) {
+		this.nome = nome;
+	}
+	
+	public String toString() {
+		return nome;
+	}
+}
